c5fbc71f89db3e57df8d07b67e66ca18b829cdfc
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-02-12 21:50:22 +0200

    removed output type from Contour

--- src/main/java/com/spikes2212/scvf/client/Contour.java
+++ src/main/java/com/spikes2212/scvf/client/Contour.java
@@ -4,13 +4,6 @@ import com.spikes2212.scvf.client.strategies.ContourMiddleware;
 
 public interface Contour {
 
-    enum OutputType {
-        PIXEL, PERCENT
-    }
-
-    void setOutputType(OutputType type);
-    OutputType getOutputType();
-
     double getX();
     double getY();
 
