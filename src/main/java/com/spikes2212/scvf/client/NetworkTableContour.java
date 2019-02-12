9f9dc50d98d1fec33f122e1aed40f59b5b3626d0
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-02-12 22:27:51 +0200

    made NetworkTableContour use Middleware

--- src/main/java/com/spikes2212/scvf/client/NetworkTableContour.java
+++ src/main/java/com/spikes2212/scvf/client/NetworkTableContour.java
@@ -1,48 +1,57 @@
 package com.spikes2212.scvf.client;
 
+import com.spikes2212.scvf.client.strategies.ContourMiddleware;
 import edu.wpi.first.networktables.NetworkTable;
+
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class NetworkTableContour implements Contour {
 
-    private NetworkTable table;
-    private int id;
-    private OutputType type;
+    private NetworkTableDataSource source;
 
-    protected NetworkTableContour(NetworkTable table, int id, OutputType type) {
-        this.table = table;
-        this.id = id;
-        setOutputType(type);
+    private List<ContourMiddleware> middlewares;
+
+    protected NetworkTableContour(NetworkTable table, int id) {
+        this.source = new NetworkTableDataSource(table, id);
+        this.middlewares = new LinkedList<>();
     }
 
-    @Override
-    public void setOutputType(OutputType type) {
-        this.type = type;
+    protected double applyMiddlewareOnXAxis(double x) {
+        double result = x;
+        for (ContourMiddleware middleware : middlewares) {
+            result = middleware.processXAxis(result);
+        }
+        return result;
     }
 
-    @Override
-    public OutputType getOutputType() {
-        return type;
+    protected double applyMiddlewareOnYAxis(double y) {
+        double result = y;
+        for (ContourMiddleware middleware : middlewares) {
+            result = middleware.processYAxis(result);
+        }
+        return result;
     }
 
     @Override
     public double getX() {
-        return table.getEntry("x" + id).getDouble(0);
+        return applyMiddlewareOnXAxis(source.getX());
     }
 
     @Override
     public double getY() {
-        return table.getEntry("y" + id).getDouble(0);
+        return applyMiddlewareOnYAxis(source.getY());
     }
 
     @Override
     public double getWidth() {
-        return table.getEntry("width" + id).getDouble(0);
+        return applyMiddlewareOnXAxis(source.getWidth());
     }
 
     @Override
     public double getHeight() {
-        return table.getEntry("height" + id).getDouble(0);
+        return applyMiddlewareOnYAxis(source.getHeight());
     }
 
     @Override
@@ -57,7 +66,12 @@ public class NetworkTableContour implements Contour {
 
     @Override
     public boolean isUpdated() {
-        return table.getEntry("isUpdated" + id).getBoolean(false);
+        return source.isUpdated();
+    }
+
+    @Override
+    public void use(ContourMiddleware middleware) {
+        middlewares.add(middleware);
     }
 
 }
