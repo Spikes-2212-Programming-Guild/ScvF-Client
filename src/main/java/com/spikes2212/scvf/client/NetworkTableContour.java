e9e74f048ba4ed283e3e6138fe13a352cbef77da
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-02-12 18:26:58 +0200

    removed scalingFunction

--- src/main/java/com/spikes2212/scvf/client/NetworkTableContour.java
+++ src/main/java/com/spikes2212/scvf/client/NetworkTableContour.java
@@ -2,43 +2,35 @@ package com.spikes2212.scvf.client;
 
 import edu.wpi.first.networktables.NetworkTable;
 
-import java.util.function.Function;
 
 public class NetworkTableContour implements Contour {
 
     private NetworkTable table;
     private int id;
 
-    private Function<Double, Double> scalingFunction;
-
-    protected NetworkTableContour(NetworkTable table, int id, Function<Double, Double> scalingFunction) {
+    protected NetworkTableContour(NetworkTable table, int id) {
         this.table = table;
         this.id = id;
-        this.scalingFunction = scalingFunction;
-    }
-
-    protected NetworkTableContour(NetworkTable table, int id) {
-        this(table, id, (d) -> d);
     }
 
     @Override
     public double getX() {
-        return scalingFunction.apply(table.getEntry("x" + id).getDouble(0));
+        return table.getEntry("x" + id).getDouble(0);
     }
 
     @Override
     public double getY() {
-        return scalingFunction.apply(table.getEntry("y" + id).getDouble(0));
+        return table.getEntry("y" + id).getDouble(0);
     }
 
     @Override
     public double getWidth() {
-        return scalingFunction.apply(table.getEntry("width" + id).getDouble(0));
+        return table.getEntry("width" + id).getDouble(0);
     }
 
     @Override
     public double getHeight() {
-        return scalingFunction.apply(table.getEntry("height" + id).getDouble(0));
+        return table.getEntry("height" + id).getDouble(0);
     }
 
     @Override
