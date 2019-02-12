9c41d8f0c5b0ee0413565e792da39d0c49650983
Simon Kharmatsky <simon.kharmatsky@gmail.com>
2019-02-12 17:44:12 +0200

    added Contour

---
+++ src/main/java/com/spikes2212/scvf/client/Contour.java
@@ -0,0 +1,14 @@
+package com.spikes2212.scvf.client;
+
+public interface Contour {
+    double getX();
+    double getY();
+
+    double getWidth();
+    double getHeight();
+
+    double getCenterX();
+    double getCenterY();
+
+    boolean isUpdated();
+}
